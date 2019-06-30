80ac7f806cfc0f32e9fce6270aa193c2c597ef72
Zet <sword-dancer@yandex.ru>
2019-06-30 17:41:48 +0300

    login methods moved to BasePage

--- test_product_page.py
+++ test_product_page.py
@@ -15,8 +15,21 @@ import pytest
 	# page.should_be_valid_cart_name()
 	# page.should_be_valid_cart_price()
 
-def test_guest_cant_see_success_message(browser):
-	link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+# def test_guest_cant_see_success_message(browser):
+	# link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+	# page = ProductPage(browser, link)
+	# page.open()
+	# page.should_not_be_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
-	page.should_not_be_success_message()
+	page.should_be_login_link()
+	
+def test_guest_can_go_to_login_page_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.go_to_login_page()
+	
