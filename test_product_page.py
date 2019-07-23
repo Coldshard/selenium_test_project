d999c59db97dac8ca646a0e7a9745434fbf5096e
Zet <sword-dancer@yandex.ru>
2019-07-23 13:40:23 +0300

    product link changed from hardcode to variable

--- test_product_page.py
+++ test_product_page.py
@@ -4,23 +4,24 @@ from .pages.cart_page import CartPage
 import pytest
 import time
 
+product_link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/'
 
 def test_guest_should_see_login_link_on_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	link = product_link
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_be_login_link()
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	link = product_link
 	page = ProductPage(browser, link)
 	page.open()
 	page.go_to_login_page()
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+	link = product_link
 	page = ProductPage(browser, link)
 	page.open()
 	page.go_to_cart_page()
@@ -30,7 +31,7 @@ def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_cart(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+	link = product_link + '?promo=newYear'
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_be_product_page()
@@ -55,7 +56,7 @@ class TestUserAddToCartFromProductPage(object):
 	
 	@pytest.mark.need_review
 	def test_user_can_add_product_to_cart(self, browser):
-		link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+		link = product_link + '?promo=newYear'
 		page = ProductPage(browser, link)
 		page.open()
 		page.should_be_product_page()
@@ -66,7 +67,7 @@ class TestUserAddToCartFromProductPage(object):
 		page.should_be_valid_cart_price()
 
 	def test_user_cant_see_success_message(self, browser):
-		link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+		link = product_link
 		page = ProductPage(browser, link)
 		page.open()
 		page.should_not_be_success_message()
